9971947fe52a2ac39838223ce7da823a4bdfe514
alfredofdlv <alfredo.florez04@csioviedo.com>
2024-11-20 13:23:31 +0000

    Feature Engineering con macroeconomia y mezcla con los datos de stocks en 1 csv

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import yfinance as yf
 import os 
+from fredapi import Fred
+API_KEY='c73684ead935d3557c5d7a2b119f903a'
+fred = Fred(api_key=API_KEY)
+
 def extract_sp500_companies(url = "https://es.wikipedia.org/wiki/Anexo:Compa%C3%B1%C3%ADas_del_S%26P_500" , export = True) : 
     # Leer todas las tablas de la página
     tables = pd.read_html(url)
@@ -17,7 +21,7 @@ def extract_stock(stock_symbol, start_date = "2010-01-01", end_date = "2024-01-3
         return None
     return data
 
-def process_stockdata(raw_data):
+def process_data(raw_data):
     """
     Procesa datos de acciones eliminando las primeras líneas no relevantes
     y configurando las columnas adecuadas.
@@ -32,7 +36,7 @@ def process_stockdata(raw_data):
     columns = ["Date", "AdjustedClose", "Close", "High", "Low", "Open", "Volume"]
     data = pd.read_csv(raw_data, skiprows=3, header=None)
     data.columns = columns
-    data['Date'] = pd.to_datetime(data['Date'], format="%d/%m/%Y")
+    data['Date'] = pd.to_datetime(data['Date'], format="ISO8601")
     
     return data
 
@@ -58,3 +62,65 @@ def join_stock_data(folder=r"data/stocks", output=r"data/final_dataset.csv",expo
     if export : 
         final_dataset.to_csv(output, index=False)
     return final_dataset
+
+def extract_macroeconomics(series,start_date = "2010-01-01", end_date = "2024-01-31"):
+    if series in [ '^GSPC','^IXIC','^RUT',    '^STOXX50E',    '^FTSE',    'CL=F',    'SI=F',
+    'GC=F',
+    '^HSI',
+    'NG=F',
+    'ZC=F',
+    'EURUSD=X']:
+            raw_macro_data=extract_stock(series,start_date,end_date)
+            if raw_macro_data is not None : 
+                raw_macro_data.to_csv(f"data/raw_macro/{series}.csv")
+                macro_data = process_data(f"data/raw_macro/{series}.csv",macro=True)
+                macro_data=macro_data[['Date','AdjustedClose']]
+                macro_data.rename(columns={'AdjustedClose':f'{series}'},inplace=True)
+                
+    else: 
+            # Obtener la serie 
+            macro_data = fred.get_series(series, observation_start=start_date, observation_end=end_date)
+            macro_data=pd.DataFrame(macro_data,columns=[f'{series}'])
+            macro_data.reset_index(names='Date',inplace=True)
+            macro_data['Date']=pd.to_datetime(macro_data['Date'],format="ISO8601")
+    data=macro_data
+    macro_data.to_csv(f"data/macro/{series}.csv")
+
+
+    return data
+
+def join_macro(series_ids,start,end):
+     # DataFrame para almacenar todos los datos de entrenamiento
+    dataframes=[]
+    for series in series_ids: 
+        try:
+            data=extract_macroeconomics(series,start,end)
+        except: 
+            print(f'Serie {series} ha fallado')
+        data['Date']=pd.to_datetime(data['Date']).dt.tz_localize(None) 
+        dataframes.append(data)
+    # Eliminar duplicados en cada DataFrame antes de concatenar
+    dataframes = [df.drop_duplicates(subset='Date') for df in dataframes]
+    final_df = pd.concat(dataframes, ignore_index=True)
+    data=final_df
+    date_range = pd.date_range(start=data['Date'].min(), end=data['Date'].max())
+
+    # Crear un DataFrame con el rango de fechas único
+    unique_dates = pd.DataFrame({'Date': date_range})
+
+    # Agrupar por fecha consolidando los valores en una fila por fecha
+    data_grouped = data.groupby('Date').first().reset_index()
+
+    # Realizar un merge con el rango de fechas para asegurar que todas las fechas estén presentes
+    final_df = pd.merge(unique_dates, data_grouped, on='Date', how='left')
+
+    # Ordenar por fecha si es necesario
+    final_df.sort_values(by='Date', inplace=True)
+    # 
+    # # Aplicar backfill a nivel de columnas
+    final_df.bfill( inplace=True)
+    final_df.ffill(inplace=True)
+
+    # Mostrar resultado
+    final_df.to_csv(f"data/macro_data.csv")
+    return final_df
